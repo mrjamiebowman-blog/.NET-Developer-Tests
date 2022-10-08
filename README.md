2bd4242a0a1d375a1f19a86b3b25824c4473614d
mrjamiebowman <mrjamiebowman@gmail.com>
2022-10-08 15:34:35 -0500

    tests, readme

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # .NET: Developer Tests
 This project will contain an abstract class and sample of how to use this class for building developer tests.
+
+## App Packages (UI)
+Application Insights (Worker)
+
+AutoMapper
+
+
+## Packages
+
+Moq
+
+Fluent Assertions
+
+## Git Ignore (appSettings.json)
+```json
+# appsettings
+**/appSettings.json
+**/appSettings.*.json
+
+# launchsettings
+**/launchSettings.json
+```
---
+++ test/MrJB.DeveloperTests.App.Tests.L5.Developer/BaseDeveloperTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrJB.DeveloperTests.App.Tests.L5.Developer;
+
+//public abstract class BaseDeveloperTest {
+//    // auto-mapper
+//    protected IMapper _autoMapper;
+
+//    // app insights
+//    protected TelemetryClient _telemetryClient;
+
+//    // configuration
+//    protected ConsumerSettings _consumerSettings;
+
+//    public BaseDeveloperTest()
+//    {
+//        LoadAppSettingsAsync().Wait();
+//        GetSetAutoMapperAsync().Wait();
+//        GetSetAppInsightsAsync().Wait();
+//    }
+
+//    protected Task LoadAppSettingsAsync()
+//    {
+//        // configuration
+//        var configurationBuilder = new ConfigurationBuilder();
+//        var path = Path.Combine(Directory.GetCurrentDirectory(), "appSettings.json");
+//        configurationBuilder.AddJsonFile(path, false);
+
+//        // build
+//        var root = configurationBuilder.Build();
+
+//        // set configuration
+//        _consumerSettings = new ConsumerSettings();
+//        root.GetSection(ConsumerSettings.Position).Bind(_consumerSettings);
+
+//        return Task.CompletedTask;
+//    }
+
+//    protected Task<IMapper> GetSetAutoMapperAsync()
+//    {
+//        // auto mapper
+//        var config = new MapperConfiguration(cfg =>
+//            cfg.AddProfile<DataServiceMappingProfiles>()
+//        );
+
+//        // mapper 
+//        _autoMapper = new Mapper(config);
+//        return Task.FromResult(_autoMapper);
+//    }
+
+//    protected Task<TelemetryClient> GetSetAppInsightsAsync(Action<TelemetryConfiguration> configAction = null)
+//    {
+//        TelemetryConfiguration configuration = TelemetryConfiguration.CreateDefault();
+//        configuration.InstrumentationKey = "";
+//        configAction?.Invoke(configuration);
+
+//        var telemetryClient = new TelemetryClient(configuration);
+//        _telemetryClient = telemetryClient;
+//        return Task.FromResult(telemetryClient);
+//    }
+//}
+
